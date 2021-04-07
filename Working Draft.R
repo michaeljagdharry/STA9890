88b0c82905f621d99b3e7ac3c17be2b736260d54
michaeljagdharry <michael.jagdharry@gmail.com>
2021-04-06 23:30:58 -0400

    Update Working Draft.R

--- Working Draft.R
+++ Working Draft.R
@@ -1,8 +1,10 @@
 setwd("C:\\Users\\leahc\\OneDrive - The City University of New York\\Masters\\Classes\\STA 9890 - Statistical Learning\\Final Project Git")
 source("Regularization Function.R")
 
-
 #Creating DFs to make boxplots
+regularize(.5)
+rbind(regularize(1),regularize(1))
+
 (R2 <- cbind(rbind(regularize(1),regularize(1)), rbind(regularize(0),regularize(0))))
 R2[c(1,3,2,4)] #If desired, we can reordering the columns like this
 
