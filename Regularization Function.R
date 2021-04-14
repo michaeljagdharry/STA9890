6244f671d50549fe622770aae5601b39c07446fb
michaeljagdharry <michael.jagdharry@gmail.com>
2021-04-14 04:42:44 -0400

    Update Regularization Function.R
    Added section headers
    Added residuals
    Added elapsed time
    Output is now a collection of items - residuals, r2's, elasped time. You can access what you want with $

--- Regularization Function.R
+++ Regularization Function.R
@@ -4,7 +4,11 @@ library(glmnet)
 
 Hitters = na.omit(Hitters)
 
-regularize <- function(a, plot=FALSE) {
+regularize <- function(a, cvplot=FALSE) {
+  
+# Cross-Validation ---  
+
+  t_start = Sys.time() #Start timer
   split = sort(sample(nrow(Hitters), size = .8*nrow(Hitters), rep=F)) #Random selection of 80% of indices
   #train = Hitters[dt,] #Random subset of data
   #test = Hitters[-dt,] #Complement of random subset of data
@@ -17,43 +21,57 @@ regularize <- function(a, plot=FALSE) {
   y.test = y[-split]
   
   lambdas = 10^seq(6,-2,length=50); lambdas #vector of lambdas to try out
-  #ridge.mod = glmnet(x,y,alpha=0,lambda=lambda_range) #Perform length(lambda_range) Ridge Regressions, each for a different lambda in lambda_range. Wow!
   #coef(ridge.mod)[,50] #Accessing the ridge regression coefficients for the 50th lambda
-  #ridge.mod.train = glmnet(x[dt,],y[dt],alpha=0,lambda=lambda_range, thresh =1e-12) #Training Models 
   
-  cv.out=cv.glmnet(x.train,y.train,alpha=a,lambda = lambdas); cv.out
-  cv.out$lambda.min
-  (bestlam=cv.out$lambda.min)
-  if (plot == TRUE) {
-    plot(cv.out) #Plots the CV(10) estimate (pg. 181) for each lambda.
+  cv.out=cv.glmnet(x.train,y.train,alpha=a,lambda = lambdas)
+  t_end = Sys.time() #End timer
+  CV_runtime = t_end - t_start
+  
+  bestlam=cv.out$lambda.min
+
+# Plotting Cross-Validation Curve ---
+
+  if (cvplot == TRUE) {
+    #Plots the CV(10) estimate (pg. 181) for each lambda.
+    if (a == 0) {plot(cv.out,sub = "10-Fold Cross Validation Curve for Ridge Regression",)}
+    else if (a==.5) {plot(cv.out,sub = "10-Fold Cross Validation Curve for El-net Regression",)}
+    else if (a==1) {plot(cv.out,sub = "10-Fold Cross Validation Curve for Lasso Regression",)}
     abline(v=log(bestlam), col="red")
   }
-  #Why are 19's appearing at the top?
-  ridge.out=glmnet(x,y,alpha=a) #Ridge Regression model with unspecified lambda(?). Documentation warns against supplying a single lamdba for prediction.
-  (best_model_coef <- predict(ridge.out,type="coefficients",s=bestlam)) #Predictions for model with cross validated lambda. Documentation advises to supply single lambda in predict()
+
+# Finding Test, Train R2 and Residuals  ---
+
+  cv.out=glmnet(x,y,alpha=a) 
+  #Ridge Regression model with unspecified lambda(?). Documentation warns
+  #against supplying a single lamdba for prediction. 
   
-  #3(d)
-  (y.hat.test <- predict(ridge.out,s=bestlam, newx = x.test)) #Predictions for model with cross validated lambda. Documentation advises to supply single lambda in predict()
+  (y.hat.test <- predict(cv.out,s=bestlam, newx = x.test)) 
+  #Predictions for model with cross validated lambda. Documentation advises to
+  #supply single lambda in predict()
   (y.bar.test <- mean(y.test))
   (SSres.test <- sum((y.test-y.hat.test)^2))
   (SStot.test <- sum((y.test-y.bar.test)^2))
   (R2.test <- 1 - SSres.test/SStot.test)
   
-  (y.hat.train <- predict(ridge.out,s=bestlam, newx = x.train)) #Predictions for model with cross validated lambda. Documentation advises to supply single lambda in predict()
+  (y.hat.train <- predict(cv.out,s=bestlam, newx = x.train)) 
   (y.bar.train <- mean(y.train))
   (SSres.train <- sum((y.train-y.hat.train)^2))
   (SStot.train <- sum((y.train-y.bar.train)^2))
   (R2.train <- 1 - SSres.train/SStot.train)
   
-  R2 <- data.frame(R2.train,R2.test);R2
+  R2 = data.frame(R2.train,R2.test)
+  if (a == 0) {colnames(R2) = c("Ridge.Train.R2","Ridge.Test.R2")}
+  else if (a==.5) {colnames(R2) = c("El-Net.Train.R2","El-Net.Test.R2")}
+  else if (a==1) {colnames(R2) = c("Lasso.Train.R2","Lasso.Test.R2")}
   
-  if (a == 0) {
-    colnames(R2) = c("Ridge.Train.R2","Ridge.Test.R2")
-  } else if (a==.5) {
-    colnames(R2) = c("El-Net.Train.R2","El-Net.Test.R2")
-  } else if (a==1) {
-    colnames(R2) = c("Lasso.Train.R2","Lasso.Test.R2")
-  }
-  
-  return(R2)
+  resid.train = y.train - y.hat.train
+  resid.test = y.test - y.hat.test
+
+# Output ---
+
+  output_list = list(`Test Residuals`=as.numeric(resid.test),
+                    `Train Residuals`=as.numeric(resid.train),
+                    `R2`=R2,
+                    `KFCV Elapsed Time` = t_end-t_start)
+  return(output_list)
 }
