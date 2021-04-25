1c044ec30b1a3fc96b8c8514231b0b90b598ac5d
ntweidner <81886334+ntweidner@users.noreply.github.com>
2021-04-25 01:24:16 -0400

    Update Working Draft.R
    for 5c:
    Elnet, Lasso, Ridge and RF models using the entire dataset (prompt "for all the data"), utilized 10-fold cv to identify lambda min
    extract importance + 3x beta, determine order according to elnet values
    
    Also I think we need to adjust 5a and 5b to model based on all the data as opposed to with test or train data.
    TO DO STILL, make final graph output prettier, add labels.  Will work on this in the morning.

--- Working Draft.R
+++ Working Draft.R
@@ -3,6 +3,7 @@ source("Regularization Function.R")
 source("Random Forest Function.R")
 load(".Rdata")
 library(latex2exp)
+library(gridExtra)
 
 rid.r2 = vector(); las.r2 = vector(); elnet.r2 = vector(); rf.r2 = vector()
 for (i in 1:100) {
@@ -78,3 +79,55 @@ r2.intervals <- cbind(rbind(int.r, int.l, int.e, int.rf), times)
 rownames(r2.intervals) = c("Ridge", "Lasso", "Elastic Net", "Random Forest")
 colnames(r2.intervals) = c("Rsq Quantile 5%", "Rsq Quantile 95%", "Runtime")
 r2.intervals
+
+##Part 5c ----
+#Present Bar plots of the coefficients
+#Use elastic-net estimated coefficients to create an order largest to smallest
+x=model.matrix (Salary~.,Hitters )[,-1]
+y=Hitters$Salary #Response vector for ridge regression via glmnet()
+p=dim(Hitters)[2]-1
+rf.hitters.out  =  randomForest(Salary~., data=Hitters, mtry= floor(sqrt(p)), importance=TRUE)
+
+cv.fit.elnet = cv.glmnet(x,y,alpha=0.5, nfolds=10)
+cv.fit.lasso = cv.glmnet(x,y,alpha=1, nfolds=10)
+cv.fit.ridge = cv.glmnet(x,y,alpha=0, fnolds=10)
+
+elnet.out=glmnet(x,y,alpha=.5, lambda = cv.fit.elnet$lambda.min)
+lasso.out=glmnet(x,y,alpha=1, lambda = cv.fit.lasso$lambda.min)
+ridge.out=glmnet(x,y,alpha=0, lambda = cv.fit.ridge$lambda.min)
+
+
+betaS.elnet             =     data.frame(c(1:p), as.vector(elnet.out$beta))
+colnames(betaS.elnet)   =     c( "feature", "value")
+
+betaS.lasso             =     data.frame(c(1:p), as.vector(lasso.out$beta))
+colnames(betaS.lasso)   =     c( "feature", "value")
+
+betaS.ridge             =     data.frame(c(1:p), as.vector(ridge.out$beta))
+colnames(betaS.ridge)   =     c( "feature", "value")
+
+betaS.rf                =     data.frame(c(1:p), as.vector(rf.hitters.out$importance))
+colnames(betaS.rf)      =     c( "feature", "value")
+
+#Use same order for Lasso, Ridge, RF, create 4x1 figure
+betaS.elnet$feature     =  factor(betaS.elnet$feature, levels = betaS.elnet$feature[order(betaS.elnet$value, decreasing = TRUE)])
+betaS.lasso$feature     =  factor(betaS.lasso$feature, levels = betaS.elnet$feature[order(betaS.elnet$value, decreasing = TRUE)])
+betaS.ridge$feature     =  factor(betaS.ridge$feature, levels = betaS.elnet$feature[order(betaS.elnet$value, decreasing = TRUE)])
+
+imp.rf <- importance(hitters.rf)
+print(imp.rf[order(imp.rf[, 1]), ])
+betaS.rf$feature        =  factor(betaS.rf$feature, levels = betaS.elnet$feature[order(betaS.elnet$value, decreasing = TRUE)])
+
+elnetPlot =  ggplot(betaS.elnet, aes(x=feature, y=value)) +
+  geom_bar(stat = "identity", fill="white", colour="black")
+ 
+lassoPlot =  ggplot(betaS.lasso, aes(x=feature, y=value)) +
+  geom_bar(stat = "identity", fill="white", colour="black")
+
+ridgePlot =  ggplot(betaS.ridge, aes(x=feature, y=value)) +
+  geom_bar(stat = "identity", fill="white", colour="black")   
+
+rfPlot =  ggplot(betaS.rf, aes(x=feature, y=value)) +
+  geom_bar(stat = "identity", fill="white", colour="black") 
+
+grid.arrange(elnetPlot, lassoPlot, ridgePlot, rfPlot, nrow = 4)
